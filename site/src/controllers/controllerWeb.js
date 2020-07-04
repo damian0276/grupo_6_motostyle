03a7dee211b46625a2e7d0cf54c96cb385698e92
Leandro Corbalan <leandrocorbalan88@hotmail.com>
2020-07-04 20:41:38 -0300

    Más correcciones y pagina de registro

--- site/src/controllers/controllerWeb.js
+++ site/src/controllers/controllerWeb.js
@@ -7,6 +7,9 @@ module.exports = {
     },
     contacto: function(req,res){
         res.sendFile(path.resolve(__dirname, '..', 'views','web','contacto.html'));   
-}
+    },
+    register: function(req, res){
+        res.sendFile(path.resolve(__dirname, '..', 'views','web','register.html'))
+    }
 
 }
