fe8b10aab1adf79c622a148e02eb89da6a926457
damian <damian0276@gmail.com>
2020-07-05 19:57:19 -0300

    Creacion de archvos administrar y edit.html

--- site/src/controllers/controllerAdmin.js
+++ site/src/controllers/controllerAdmin.js
@@ -2,13 +2,18 @@ const path = require('path');
 const fs = require('fs')
 
 module.exports = {
-    add: function(req,res){
+
+    add: (req,res) =>{
         res.sendFile(path.resolve(__dirname, '..', 'views','admin','productAdd.html'));
     },
      edit: (req,res) =>{
-        //  let motos = JSON.parse(fs.readFileSync(path.resolve(__dirname,'..','data',motos.json)));
-    //     const motosId = req.params.id;
-    //    let motosEditar = motos.find(moto =>moto.id == motosId)
+    
        res.sendFile(path.resolve(__dirname,'..','views','admin','edit.html'));
+     },
+     administrar: (req,res) =>{
+        res.sendFile(path.resolve(__dirname,'..','views','admin','administrar.html'));
+
      }
+
+    
 }
--- site/src/routes/admin.js
+++ site/src/routes/admin.js
@@ -4,6 +4,7 @@ const path = require('path');
 
 const controllerAdmin = require(path.resolve(__dirname, '..', 'controllers', 'controlleradmin'));
 
+router.get('/administrar', controllerAdmin.administrar);
 router.get('/productAdd', controllerAdmin.add);
 router.get('/edit', controllerAdmin.edit);
 
