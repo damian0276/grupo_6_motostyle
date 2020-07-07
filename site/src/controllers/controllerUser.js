9513fb1afac5e413b223f48dba2e91ed43d059dc
China Fernandes <chinafernandess@gmail.com>
2020-07-07 16:04:29 -0300

    password recovery

--- site/src/controllers/controllerUser.js
+++ site/src/controllers/controllerUser.js
@@ -4,6 +4,9 @@ const fs = require('fs');
 module.exports = {    
     register: function(req, res){
         res.sendFile(path.resolve(__dirname, '..', 'views','users','register.html'))
-    }
+    },
+    passwordRecovery: function(req,res){
+        res.sendFile(path.resolve(__dirname, '..', 'views','users','passwordRecovery.html'));   
+    }  
 }
  
