5767fe2727ecf9c801f364ec8b4a72c38aec4903
Leandro Corbalan <leandrocorbalan88@hotmail.com>
2020-07-15 21:57:07 -0300

    files.json, productdetail, products e index dinami

--- site/src/controllers/controllerAdmin.js
+++ site/src/controllers/controllerAdmin.js
@@ -2,19 +2,20 @@ const path = require('path');
 const fs = require('fs')
 
 module.exports = {
-
-    add: (req,res) =>{
-        res.render(path.resolve(__dirname, '..', 'views','admin','productAdd'));
-    },
-     edit: (req,res) =>{
-    
-       res.render(path.resolve(__dirname,'..','views','admin','edit'));
-     },
-     administrar: (req,res) =>{
-        res.render(path.resolve(__dirname,'..','views','admin','administrar'));
-
-     }
-    
-
-    
+  motos: function(){ 
+    return JSON.parse(fs.readFileSync(path.resolve(__dirname, '../data/products.json')));
+  },
+  guardar: function(data){
+    return fs.writeFileSync(path.resolve(__dirname,'../data/products.json'),JSON.stringify(data, null, 2)); 
+  },
+  add: (req,res) =>{
+      res.render(path.resolve(__dirname, '..', 'views','admin','productAdd'));
+  },
+   edit: (req,res) =>{
+  
+     res.render(path.resolve(__dirname,'..','views','admin','edit'));
+   },
+   administrar: (req,res) =>{
+      res.render(path.resolve(__dirname,'..','views','admin','administrar'));
+  }   
 }
--- site/src/controllers/controllerProduct.js
+++ site/src/controllers/controllerProduct.js
@@ -1,14 +1,17 @@
 const path = require('path');
 const fs = require('fs')
 
+let motos = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../data/products.json')));
+
 module.exports = {
     index: function(req,res){
-        res.render(path.resolve(__dirname, '..', 'views','products','products'));
+        res.render(path.resolve(__dirname, '..', 'views','products','products'),{motos});
     },
     cart: function(req,res){
         res.render(path.resolve(__dirname, '..', 'views','products','productCart'));
     },
     detail: function(req,res){
-        res.render(path.resolve(__dirname, '..', 'views','products','productDetail'));
+        let motoAMostrar = motos.find(moto => moto.id == req.params.id);
+        res.render(path.resolve(__dirname, '..', 'views','products','productDetail'), {moto: motoAMostrar});
     }
 }
