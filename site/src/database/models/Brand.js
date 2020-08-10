a1d0b00597f8cfb47a2e271fb56d31a2ee7e810f
damian <damian0276@gmail.com>
2020-08-09 23:02:25 -0300

    corregi errores en brand,image,

--- site/src/database/models/Brand.js
+++ site/src/database/models/Brand.js
@@ -1,30 +1,30 @@
-// module.exports = (sequelize,DataType) =>{
-//     let alias = 'Brand';
-//     let cols ={
-//         id: {
-//             type: DataType.INTEGER,
-//             primaryKey : true,
-//             autoIncrement:true,
-//             allowNull:false
-//         },
-//         name:{
-//             type: DataType.STRING(100),
-//             allowNull:false
-//         },
+ module.exports = (sequelize,DataTypes) =>{
+    let alias = 'Brand';
+    let cols ={
+         id: {
+           type: DataTypes.INTEGER,
+             primaryKey : true,
+             autoIncrement:true,
+             allowNull:false
+         },
+         name:{
+             type: DataTypes.STRING(100),
+             allowNull:false
+        },
 
-//     }
-//     let config = {
-//         tableName: 'brands'
-//     }
-//     //Asocio tabla brands con la tabla products
-//     const Brand = sequelize.define(alias,cols,config)
-//     Brand.associate = function(models){
-//         Brand.hasMany(models.Product,
-//             {
-//                 as:'brands',
-//                 foreugnKey:'brandId'
-//             }
-//             )        
-//     }
-//     return Brand;
-// }
+    }
+     let config = {
+        tableName: 'brands'
+     }
+    //Asocio tabla brands con la tabla products
+     const Brand = sequelize.define(alias,cols,config)
+    Brand.associate = function(models){
+        Brand.hasMany(models.Product,
+             {
+                 as:'products',
+                 foreignKey:'brandId'
+             }
+             )        
+     }
+     return Brand;
+ }
--- site/src/database/models/Image.js
+++ site/src/database/models/Image.js
@@ -1,28 +1,32 @@
-// module.exports = (sequelize, DataType)=>{
-//     let alias = 'Image';
-//     let cols = {
-//        id: {
-//             type: DataType.INTEGER,
-//             primaryKey : true,
-//             autoIncrement:true,
-//             allowNull:false
-//            },
-//         name:{
-//             type: DataType.STRING(500),
-//             allowNull:false
-//             },
-//     }
-//     let config = {
-//         tableName ='images'
-//     };
-//     const Image = sequelize.difine(alias,cols,config)
-//     Image.associate = function(models){
-//         Imge.hasMany(models.imagePoduct,{
-//             as:'imagePoduct',
-//             foreignKey: 'imageId'
-//         })
-//     }
-    
-//     return Image;
+ module.exports = (sequelize, DataTypes)=>{
+     let alias = 'Image';
+     let cols = {
+        id: {
+             type: DataTypes.INTEGER,
+             primaryKey : true,
+             autoIncrement:true,
+             allowNull:false
+            },
+         name:{
+             type: DataTypes.STRING(500),
+             allowNull:false
+             },
+     }
+     let config = {
+         tableName:'images'
+     };
+     const Image = sequelize.define(alias,cols,config)
+     Image.associate = function(models){
+         Image.belongsTo(models.Poduct,{
+             as:'products',
+             through:'imageProduct',
+             foreignKey: 'imageId',
+             otherKey:'productId'
 
-// }
+
+         })
+     }
+    
+     return Image;
+
+ }
