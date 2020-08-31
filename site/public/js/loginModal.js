4627e0f9aca3de5919c62a115686bc1c666bfbf6
damian <damian0276@gmail.com>
2020-08-31 16:59:30 -0300

    correccion de loginModal

--- site/public/js/loginModal.js
+++ site/public/js/loginModal.js
@@ -14,14 +14,14 @@ window.addEventListener('load',function(){
        
           //Destructuring  
           let {email, password } = formulario.elements;
-          let errores = [];
+          let errors = [];
           console.log(formulario.elements.email);
         //Validar el email - Expresiones Regulares https://www.w3schools.com/jsref/jsref_obj_regexp.asp       https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
 
         let reEmail  = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
   
         if(!reEmail.test(email.value)){
-            errores.push('El email es inválido...');
+            errors.push('El email es inválido...');
            // errores['email'] = 'El campo nombre no puede estar vacio...';
             email.classList.add('input-invalid'); 
               
@@ -33,7 +33,7 @@ window.addEventListener('load',function(){
        //Aquí valido el password haciendo uso de Expresiones Regulares
        //Esta expresión regular valida como Mínimo seis caracteres, al menos una letra y un número:
         let rePassword = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/
-        if(!rePassword.test(password.value)){errores.push('La contraseña como mínimo debe tener ocho caracteres, al menos una letra y un número');
+        if(!rePassword.test(password.value)){errors.push('La contraseña como mínimo debe tener ocho caracteres, al menos una letra y un número');
            password.classList.add('input-invalid');   
           // errores['password'] = 'El campo nombre no puede estar vacio...';
          //password.value.length <8 
@@ -43,16 +43,16 @@ window.addEventListener('load',function(){
       }
 
          //Aquí enviamos los errores al usuario
-         let ulErrores = document.getElementById('errores');
+         let ulErrores = document.getElementById('errors');
          ulErrores.classList.add('alert-danger')
-         if(errores.length > 0){
+         if(errors.length > 0){
              
              evento.preventDefault();
              ulErrores.innerHTML = "";
-             for (let i = 0 ; i < errores.length; i++){
-               ulErrores.innerHTML += `<li> ${errores[i]} </li> `
+             for (let i = 0 ; i < errors.length; i++){
+               ulErrores.innerHTML += `<li> ${errors[i]} </li> `
              }
-             errores = [];
+             errors = [];
          }else{
              return true;
          } 
